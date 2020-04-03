3d8ddf25cf8e6509215d766d467351d56ad13c44
Bao Xian TSANG <bxtsang.2018@sis.smu.edu.sg>
2020-04-03 18:50:44 +0800

    notify

--- notify/notify.py
+++ notify/notify.py
@@ -6,10 +6,10 @@ import smtplib, ssl
 
 app = Flask(__name__)
 CORS(app)
-
+port = 5200
 
 class Telegram:
-    
+
     def __init__(self):
 
         with open ('api_token.txt', 'rt') as file:
@@ -39,9 +39,10 @@ def send_notif():
     promo = data['promo']
     tiers = promo['tiers']
     tiers_str = ''.join([str(tier) for tier in tiers])
-    
-    r = requests.get("http://127.0.0.1:5001/ESDproject/view?tier={}".format(tiers_str))
-    to_send = r.json()
+
+    query = {"query": "query { getCustomers (tier:" + tiers_str + ") { userID, name, email, teleID } }" }
+    r = requests.post("http://g6t3esd.team:8000/customer", json = query)
+    to_send = r.json()["data"]["getCustomers"]
 
     details = "promo code: {} \ndiscount: {} \nvalid period: {} - {} \nnumber of redemptions: {}".format(promo['code'], promo['discount'], promo['start'], promo['end'], promo['redemptions'])
 
@@ -54,19 +55,20 @@ def send_notif():
         tele_fail = []
 
         for customer in to_send:
-            r = tele.send_msg(customer['tele_id'], message)
+            r = tele.send_msg(customer['teleID'], message)
             print(r)
             if r != 200:
-                tele_fail.append(customer)
-    
+                tele_fail.append(customer["userID"])
+
         fail["telegram"] = tele_fail
 
 
     if "email" in channels:
         port = 465  # For SSL
         smtp_server = "smtp.gmail.com"
-        sender_email = "jpiswatchingyou@gmail.com"  # Enter your address
-        password = "jiapengiswatchingyou123"
+        sender_email = "oiladders@gmail.com"  # Enter your address
+        with open ('password.txt', 'rt') as file:
+            password = file.read()
         email_fail = []
 
         context = ssl.create_default_context()
@@ -77,7 +79,7 @@ def send_notif():
                     server.login(sender_email, password)
                     server.sendmail(sender_email, customer["email"], "Subject:" + message)
             else:
-                email_fail.append(customer)
+                email_fail.append(customer["email"])
 
         fail["email"] = email_fail
 
@@ -85,4 +87,4 @@ def send_notif():
 
 
 if __name__ == "__main__":
-    app.run(port=5200, debug=True)
+    app.run(port=port, debug=True)
